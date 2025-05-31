685efd42817988a9b5b3f8af5d9ea39255cc42df
임종혁 <limjonghyuk@imjonghyeog-ui-MacBookAir.local>
2025-05-31 14:21:06 +0900

    Deadmap

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
-
+import {View} from 'react-native';
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
